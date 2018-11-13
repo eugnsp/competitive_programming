d028ecc8933a673b939e95b1be82b6ed222c2f32
Es <eugns.p@gmail.com>
2018-11-13 21:49:47 +0300

    Update src/10282_babelfish.cpp

--- src/10282_babelfish.cpp
+++ src/10282_babelfish.cpp
@@ -4,22 +4,22 @@ Babelfish
 UVa ID: 102 82
 
 You have just moved from Waterloo to a big city. The people here speak
-an incomprehensible dialect of a foreign language. Fortunately, you 
+an incomprehensible dialect of a foreign language. Fortunately, you
 have a dictionary to help you understand them.
 
 Input
 -----
-Input consists of up to 100,000 dictionary entries, followed by a 
+Input consists of up to 100,000 dictionary entries, followed by a
 blank line, followed by a message of up to 100,000 words. Each
 dictionary entry is a line containing an English word, followed by a
 space and a foreign language word. No foreign word appears more than
-once in the dictionary. The message is a sequence of words in the 
-foreign language, one word on each line. Each word in the input is a 
+once in the dictionary. The message is a sequence of words in the
+foreign language, one word on each line. Each word in the input is a
 sequence of at most 10 lowercase letters.
 
 Output
 ------
-Output is the message translated to English, one word per line. 
+Output is the message translated to English, one word per line.
 Foreign words not in the dictionary should be translated as 'eh'.
 
 This file is covered by the LICENSE file in the root of this project.
@@ -33,27 +33,34 @@ This file is covered by the LICENSE file in the root of this project.
 #include <vector>
 #include <utility>
 
-template<class Key, class Value>
+using Word = std::string;
+using Words_pair = std::pair<Word, Word>;
+
+// An open addressing hash table with quadratic probing
 class Hash_table
 {
 private:
+	using Key = Word;
+	using Value = Word;
 	using Pair = std::pair<Key, Value>;
 
 public:
 	Hash_table(std::size_t size)
 	{
-		++size;
-		size *= 1.5;
+		// Get the next power of two
+		while (size & (size - 1))
+			++size;
+
 		table_.resize(size);
 	}
 
 	void insert(Pair pair)
 	{
-		const auto hash = std::hash<Key>{}(pair.first);
+		assert(!pair.second.empty());
+
 		const auto size = table_.size();
-		
-		auto pos = hash % size;
-		while (true)
+		auto pos = hash(pair.first);
+		for (std::size_t i = 1; i <= size; ++i)
 		{
 			if (table_[pos].second.empty())
 			{
@@ -61,33 +68,35 @@ public:
 				return;
 			}
 
-			++pos;
-			if (pos == size)
-				pos = 0;
+			pos = (pos + i) % size;
 		}
+
+		assert(false);
 	}
 
 	const Value* operator[](const Key& key) const
 	{
-		const auto hash = std::hash<Key>{}(key);
 		const auto size = table_.size();
-		
-		auto pos = hash % size;
-		while (true)
+		auto pos = hash(key);
+		for (std::size_t i = 1; i <= size; ++i)
 		{
 			if (table_[pos].first == key)
 				return &table_[pos].second;
 			if (table_[pos].second.empty())
 				return nullptr;
 
-			++pos;
-			if (pos == size)
-				pos = 0;
+			pos = (pos + i) % size;
 		}
+
+		assert(false);
+		return nullptr;
 	}
 
 private:
-
+	std::size_t hash(const Key& key) const
+	{
+		return std::hash<Key>{}(key) % table_.size();
+	}
 
 private:
 	std::vector<Pair> table_;
@@ -114,7 +123,7 @@ private:
 
 	virtual void solve(unsigned int) override
 	{
-		Hash_table<std::string, std::string> table(dictionary_.size());
+		Hash_table table(dictionary_.size());
 		for (auto& words : dictionary_)
 			table.insert(swap_words(std::move(words)));
 
@@ -128,7 +137,7 @@ private:
 		}
 	}
 
-	static std::pair<std::string, std::string> split(std::string str)
+	static Words_pair split(std::string str)
 	{
 		const auto space_pos = std::find(str.begin(), str.end(), ' ');
 		assert(space_pos != str.end());
@@ -139,14 +148,14 @@ private:
 		return {std::move(str), std::move(second_word)};
 	}
 
-	static std::pair<std::string, std::string> swap_words(std::pair<std::string, std::string> p)
+	static Words_pair swap_words(Words_pair p)
 	{
 		return {std::move(p.second), std::move(p.first)};
 	}
 
 private:
-	std::vector<std::pair<std::string, std::string>> dictionary_;
-	std::vector<std::string> words_;
+	std::vector<Words_pair> dictionary_;
+	std::vector<Word> words_;
 };
 
 MAIN(CP)
